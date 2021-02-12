b4fb4bba838ebad3fb3fdb2c3a6f34f77ae47705
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-02-12 17:52:50 +0900

    spring -> 이메일, 닉네임 중복체크하는 GET 함수, 회원가입해서 저장하는 POST 함수 작성

--- Spring/src/main/java/CarrotMarket/CarrotMarket/controller/MemberController.java
+++ Spring/src/main/java/CarrotMarket/CarrotMarket/controller/MemberController.java
@@ -5,16 +5,21 @@ import CarrotMarket.CarrotMarket.repository.MemberRepository;
 import CarrotMarket.CarrotMarket.service.MailSender;
 import CarrotMarket.CarrotMarket.service.MemberService;
 import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.Message;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MemberController {
 
-    private MemberService memberService;
+    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
+    private final MemberService memberService;
     public MemberRepository memberRepository;
 
     @Autowired
@@ -49,12 +54,52 @@ public class MemberController {
         return result;
     }
 
-    @GetMapping("/register/emailcheck")
-    public Boolean emailcheck(@RequestParam String email){
-        Member member = memberRepository.load(email);
-        if(member.getEmail() != null){
-            return true;
+    @RequestMapping(value = "/register/emailing/{email}",method = RequestMethod.GET)
+    @ResponseBody
+    public Member emailing(@PathVariable String email){
+        Member member = new Member();
+        if(memberService.id_check(email)){
+            member.setEmail(email);
         }
-        return false;
+        else{
+            member.setEmail(null);
+        }
+
+        return member;
+    }
+
+    @RequestMapping(value = "/register/nicknaming/{nickname}",method = RequestMethod.GET)
+    @ResponseBody
+    public Member nicknaming(@PathVariable String nickname){
+        Member member = new Member();
+        if(memberService.nickname_check(nickname)){
+            member.setNickname(nickname);
+        }
+        else{
+            member.setNickname(null);
+        }
+
+        return member;
+    }
+
+    @RequestMapping(value = "/register/member",method = RequestMethod.POST)
+    public Member join(HttpServletRequest request){
+        Member member = new Member();
+        try{
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            String  name = request.getParameter("name");
+            String nickname = request.getParameter("nickname");
+            String location = request.getParameter("location");
+
+            Member member1 = new Member(email,password,name,nickname,location);
+            memberService.join(member1);
+
+            return member;
+
+        }catch (Exception e){
+            logger.info("저장중 오류 발생");
+        }
+        return member;
     }
 }
