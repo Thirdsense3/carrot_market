555f2bf3f3dda247513d7d46552680c1a5ff545b
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-02-12 17:50:56 +0900

    android -> data class member 수정, main activity 잠시 registeractivity로 변경

--- Android/app/src/main/java/com/example/carrotmarket/Member.kt
+++ Android/app/src/main/java/com/example/carrotmarket/Member.kt
@@ -3,9 +3,9 @@ package com.example.carrotmarket
 import java.io.Serializable
 
 data class Member(
-        var email : String,
-        var password : String,
-        var name : String,
-        var nickname : String,
-        var location : String
-) : Serializable
+        var email: String,
+        var password: String,
+        var name: String,
+        var location: String,
+        var nickname: String
+)
