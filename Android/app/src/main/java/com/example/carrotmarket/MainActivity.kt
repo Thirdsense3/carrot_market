8e4b8757d834531ca32608b2fdae5ca50ef28a25
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-02-23 19:20:36 +0900

    Android -> mainactivity : 진입시 로그 추가

--- Android/app/src/main/java/com/example/carrotmarket/MainActivity.kt
+++ Android/app/src/main/java/com/example/carrotmarket/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.carrotmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.e(TAG,"메인 액티비티 ")
     }
 }
