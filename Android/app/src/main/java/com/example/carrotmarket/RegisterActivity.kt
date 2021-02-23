28eea2965b957e46c270abf20aa6adea9e141817
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-02-23 14:21:20 +0900

    Android : 회원가입, 이메일 코드, 회원가입 확인 함수 코루틴 적용

--- Android/app/src/main/java/com/example/carrotmarket/RegisterActivity.kt
+++ Android/app/src/main/java/com/example/carrotmarket/RegisterActivity.kt
@@ -10,27 +10,33 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import com.example.carrotmarket.dto.CertificationCode
 import com.example.carrotmarket.dto.Member
 import com.example.carrotmarket.network.RetrofitClient
 import com.example.carrotmarket.network.RetrofitService
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import java.lang.Exception
 import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
 
     private val TAG = "RegisterActivity"
-    lateinit var retrofit : Retrofit
-    lateinit var myAPI : RetrofitService
+    lateinit var retrofit: Retrofit
+    lateinit var myAPI: RetrofitService
     lateinit var member: Member
-    lateinit var certificationcode : CertificationCode
+    lateinit var certificationcode: CertificationCode
+
+    sealed class Result<out R>{
+        data class Success<out T>(val data: T) : Result<T>()
+        data class Error(val exception: Exception) : Result<Nothing>()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,25 +52,24 @@ class RegisterActivity : AppCompatActivity() {
         val nicknameEvent = findViewById<Button>(R.id.nickNameButton)
         val joinEvent = findViewById<Button>(R.id.joinButton)
 
-        var pwCheck : Boolean = false
-        var emailCheck : Boolean = false
-        var nnCheck:Boolean =false
+        var pwCheck: Boolean = false
+        var emailCheck: Boolean = false
+        var nnCheck: Boolean = false
 
 
-        authEvent.setOnClickListener{
+        authEvent.setOnClickListener {
 
             val inputEmail = findViewById<EditText>(R.id.emailEditText).text.toString()
 
             Log.v(TAG, "email : $inputEmail")
 
             val serverCheck = authCk(inputEmail)
-            if(serverCheck){
-                Toast.makeText(this@RegisterActivity, "사용가능한 이메일입니다.",Toast.LENGTH_SHORT).show()
+            if (serverCheck) {
+                Toast.makeText(this@RegisterActivity, "사용가능한 이메일입니다.", Toast.LENGTH_SHORT).show()
                 emailCheck = true;
                 email = inputEmail
-            }
-            else{
-                Toast.makeText(this@RegisterActivity, "중복된 이메일 입니다.",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@RegisterActivity, "중복된 이메일 입니다.", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -74,33 +79,31 @@ class RegisterActivity : AppCompatActivity() {
 
             val serverCheck = nicknameCk(inputNickname)
 
-            if(serverCheck){
-                Toast.makeText(this@RegisterActivity, "사용가능한 닉네임입니다.",Toast.LENGTH_SHORT).show()
+            if (serverCheck) {
+                Toast.makeText(this@RegisterActivity, "사용가능한 닉네임입니다.", Toast.LENGTH_SHORT).show()
                 nnCheck = true
                 nn = inputNickname
-            }
-            else{
-                Log.d(TAG,serverCheck.toString())
-                Toast.makeText(this@RegisterActivity, "중복된 닉네임입니다.",Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d(TAG, serverCheck.toString())
+                Toast.makeText(this@RegisterActivity, "중복된 닉네임입니다.", Toast.LENGTH_SHORT).show()
             }
         }
 
         joinEvent.setOnClickListener {
             Log.d(TAG, "$pwCheck $emailCheck $nnCheck")
-            if(pwCheck&&emailCheck&&nnCheck) {
+            if (pwCheck && emailCheck && nnCheck) {
                 val pw = findViewById<EditText>(R.id.passwordEditText).text.toString()
                 val name = findViewById<EditText>(R.id.nameEditText).text.toString()
                 val location = findViewById<EditText>(R.id.locationEditText).text.toString()
 
-                register(email,pw,name,nn,location)
-            }
-            else{
+                register(email, pw, name, nn, location)
+            } else {
                 Toast.makeText(this@RegisterActivity, "잘못된 접근입니다.", Toast.LENGTH_SHORT).show()
             }
             //TODO ("'Member'에 저장된 애들 로그인 화면으로 넘겨주기")
         }
 
-        ckpw.addTextChangedListener(object: TextWatcher{
+        ckpw.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
@@ -108,11 +111,10 @@ class RegisterActivity : AppCompatActivity() {
                 val pw1: String = findViewById<EditText>(R.id.passwordEditText).text.toString()
                 val pw2: String = findViewById<EditText>(R.id.checkPasswordEditText).text.toString()
 
-                if(pw1 == pw2 && pw1.isNotEmpty()){
+                if (pw1 == pw2 && pw1.isNotEmpty()) {
                     pwCheck = true
                     textpw.setText("비밀번호 일치")
-                }
-                else{
+                } else {
                     pwCheck = false
                     textpw.setText("비밀번호 불일치")
                 }
@@ -124,127 +126,139 @@ class RegisterActivity : AppCompatActivity() {
 
     }
 
-    private fun authCk(email:String): Boolean{
+    class SyncViewModel(): ViewModel(){
+
+    }
+
+    private fun authCk(email: String): Boolean {
         retrofit = RetrofitClient.getInstance()
         myAPI = retrofit.create(RetrofitService::class.java)
-        var emailCheck:String ?= null
+        var emailCheck: String? = null
 
-            myAPI.emailing(email).enqueue(object : Callback<Member> {
-                override fun onFailure(call: Call<Member>, t: Throwable) {
-                    Log.d(TAG, t.message)
+        myAPI.emailing(email).enqueue(object : Callback<Member> {
+            override fun onFailure(call: Call<Member>, t: Throwable) {
+                Log.d(TAG, t.message)
+            }
+
+            override fun onResponse(call: Call<Member>, response: Response<Member>) {
+                response.body()?.let {
+                    emailCheck = it.email
                 }
+                        ?: Toast.makeText(this@RegisterActivity, "Body is null", Toast.LENGTH_SHORT).show()
+            }
+        })
 
-                override fun onResponse(call: Call<Member>, response: Response<Member>) {
-                    response.body()?.let {
-                        emailCheck = it.email
-                    } ?: Toast.makeText(this@RegisterActivity, "Body is null", Toast.LENGTH_SHORT).show()
-                }
-            })
+        Log.v(TAG, "이메일 확인 : $emailCheck")
 
-        Log.v(TAG,"이메일 확인 : $emailCheck")
-
-        if(emailCheck == null){
+        if (emailCheck == null) {
             return true
         }
 
         return false
     }
 
-    private fun nicknameCk(nickname: String): Boolean{retrofit = RetrofitClient.getInstance()
+    private fun nicknameCk(nickname: String): Boolean {
+        retrofit = RetrofitClient.getInstance()
         myAPI = retrofit.create(RetrofitService::class.java)
-        var nicknameCheck:String ?= null
+        var nicknameCheck: String? = null
 
-            myAPI.nicknaming(nickname).enqueue(object : Callback<Member> {
-                override fun onFailure(call: Call<Member>, t: Throwable) {
-                    Log.d(TAG, t.message)
+        myAPI.nicknaming(nickname).enqueue(object : Callback<Member> {
+            override fun onFailure(call: Call<Member>, t: Throwable) {
+                Log.d(TAG, t.message)
+            }
+
+            override fun onResponse(call: Call<Member>, response: retrofit2.Response<Member>) {
+                response.body()?.let {
+                    nicknameCheck = it.email
                 }
+                        ?: Toast.makeText(this@RegisterActivity, "Body is null", Toast.LENGTH_SHORT).show()
+            }
+        })
 
-                override fun onResponse(call: Call<Member>, response: retrofit2.Response<Member>) {
-                    response.body()?.let {
-                        nicknameCheck = it.email
-                    }?: Toast.makeText(this@RegisterActivity, "Body is null", Toast.LENGTH_SHORT).show()
-                }
-            })
+        Log.v(TAG, "닉네임 확인 : $nicknameCheck")
 
-        Log.v(TAG,"닉네임 확인 : $nicknameCheck")
-
-        if(nicknameCheck != null){
+        if (nicknameCheck != null) {
             return false
         }
 
         return true
     }
 
-    private suspend fun join(email:String, pw:String, name: String, nickname: String, location: String){
+    private suspend fun join(email: String, pw: String, name: String, nickname: String, location: String) {
         retrofit = RetrofitClient.getInstance()
         myAPI = retrofit.create(RetrofitService::class.java)
 
         Log.v(TAG, "join 실행")
 
-            myAPI.signUp(email, pw, name, nickname, location).enqueue(object : Callback<Member> {
-                override fun onFailure(call: Call<Member>, t: Throwable) {
-                    Log.d(TAG, t.message)
-                }
+        myAPI.signUp(email, pw, name, nickname, location).enqueue(object : Callback<Member> {
+            override fun onFailure(call: Call<Member>, t: Throwable) {
+                Log.d(TAG, t.message)
+            }
 
-                override fun onResponse(call: Call<Member>, response: retrofit2.Response<Member>) {
-                    response.body()?.let {
-                        member = Member(it.email, it.password, it.name, it.location, it.nickname)
-                    }
+            override fun onResponse(call: Call<Member>, response: retrofit2.Response<Member>) {
+                response.body()?.let {
+                    member = Member(it.email, it.password, it.name, it.location, it.nickname)
                 }
-            })
+            }
+        })
 
         Log.v(TAG, "join 끝남")
     }
 
-    private suspend fun verifying(email: String){
+    private suspend fun verifying(email: String) {
         retrofit = RetrofitClient.getInstance()
         myAPI = retrofit.create((RetrofitService::class.java))
-        Log.v(TAG,"verifying 실행 인증메일 보냄")
+        Log.v(TAG, "verifying 실행 인증메일 보냄")
 
-
+        CoroutineScope(IO).launch{
             myAPI.verifying(email).enqueue(object : Callback<CertificationCode> {
                 override fun onFailure(call: Call<CertificationCode>, t: Throwable) {
                     Log.d(TAG, t.message)
-                    Log.v(TAG,"연결 실패")
+                    Log.v(TAG, "연결 실패")
                 }
 
                 override fun onResponse(call: Call<CertificationCode>, response: retrofit2.Response<CertificationCode>) {
                     response.body()?.let {
                         certificationcode = CertificationCode(it.code)
-                        Log.v(TAG,"인증메일 받음 : ${certificationcode.code}")
+                        Log.v(TAG, "인증메일 받음 : ${certificationcode.code}")
                     }?: Toast.makeText(this@RegisterActivity, "Body is null", Toast.LENGTH_SHORT).show()
+
                 }
             })
 
-
-        Log.v(TAG, "verifying 끝남")
+            Log.v(TAG, "verifying 끝남")
+        }
     }
 
-    private fun register(email: String,pw: String, name: String, nickname: String, location: String){
+    private suspend fun checkAndRegister(){
+        val nextIntent = Intent(this@RegisterActivity, VerifyingEmailActivity::class.java)
+
+        if (this@RegisterActivity::certificationcode.isInitialized) {
+            Log.v(TAG, "emailCode : ${certificationcode.code}")
+            nextIntent.putExtra("emailCode", certificationcode.code)
+            startActivity(nextIntent) // 화면 전환
+        } else {
+            Log.v(TAG, "초기화 안됨")
+            Toast.makeText(this@RegisterActivity, "잘못된 접근입니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun register(email: String, pw: String, name: String, nickname: String, location: String) {
         /**
          * Coroutine 이용해서 초기화후에 다음코드 돌아가게 작성
          * */
 
-        GlobalScope.launch {
-            async {
-                join(email, pw, name, nickname, location)
-            }.await()
-            async {
-                verifying(email)
-            }.await()
-            async {
-                val nextIntent = Intent(this@RegisterActivity, VerifyingEmailActivity::class.java)
+        CoroutineScope(IO).launch {
+            join(email, pw, name, nickname, location)
+            verifying(email)
 
-                if (this@RegisterActivity::certificationcode.isInitialized) {
-                    nextIntent.putExtra("emailCode", certificationcode.code)
-                    startActivity(nextIntent) // 화면 전환
-                } else {
-                    Log.v(TAG, "초기화 안됨")
-                    Toast.makeText(this@RegisterActivity, "잘못된 접근입니다.", Toast.LENGTH_SHORT).show()
-                }
-            }.await()
+            val job = CoroutineScope(Main).launch {
+                checkAndRegister()
+            }
+            job.join()
         }
-
     }
 
 }
+
