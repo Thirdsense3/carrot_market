7ac43ec5182b58530a2a67a17c12e2d90df8d0ec
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-03-02 20:55:54 +0900

    Spring -> ListController, ListService : list loading 함수 작성 및 list<board>로 반환 함수 작성중

--- Spring/src/main/java/CarrotMarket/CarrotMarket/controller/ListController.java
+++ Spring/src/main/java/CarrotMarket/CarrotMarket/controller/ListController.java
@@ -1,7 +1,47 @@
 package CarrotMarket.CarrotMarket.controller;
 
+import CarrotMarket.CarrotMarket.domain.Board;
+import CarrotMarket.CarrotMarket.domain.CertificationCode;
+import CarrotMarket.CarrotMarket.service.ListService;
+import com.google.gson.Gson;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 public class ListController {
+    private static final Logger logger = LoggerFactory.getLogger(ListController.class);
+
+    private final ListService listService;
+
+    @Autowired
+    public ListController(ListService listService){
+        this.listService = listService;
+    }
+
+    @RequestMapping(value = "board/list",method = RequestMethod.GET)
+    @ResponseBody
+    public List<Board> boardList(){
+        List<Board> boardList = listService.getBoardList();
+//        JSONObject jsonObject = new JSONObject();
+//        try{
+//            JSONArray jsonArray = new JSONArray();
+//            for(int i=0;i<boardList.size();i++){
+//                JSONObject jsonObject1 = new JSONObject();
+//                jsonObject1.put("id",)
+//            }
+//        }
+        return boardList;
+    }
 }
--- Spring/src/main/java/CarrotMarket/CarrotMarket/service/ListService.java
+++ Spring/src/main/java/CarrotMarket/CarrotMarket/service/ListService.java
@@ -2,25 +2,35 @@ package CarrotMarket.CarrotMarket.service;
 
 import CarrotMarket.CarrotMarket.domain.Board;
 import CarrotMarket.CarrotMarket.repository.BoardRepository;
+import CarrotMarket.CarrotMarket.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ListService {
-    BoardRepository boardRepository;
 
-    public void getBoardList(){
-        //TODO ("게시물 해당 지역 불러오기")
-        List<Board> boardList = (List<Board>) boardRepository.load();
+    @Autowired
+    private final BoardRepository boardRepository;
+
+    public ListService(BoardRepository boardRepository) {this.boardRepository = boardRepository;}
+
+    public List<Board> getBoardList(){
+        List<Board> boardList = boardRepository.load();
+        return boardList;
     }
 
     public void getBoard(){
         //TODO ("게시물 열람 및 조회수 증가")
     }
 
-    public void searchBoardList(){
-        //TODO ("게시물 카테고리 검색")
-        //TODO ("게시물 제목 검색")
+    public void deleteBoard(){
+        //TODO ("게시물 삭제")
     }
 
+    public void autoDeleteBoard(){
+        //TODO ("게시물 유효기간 만료로 인한 삭제)
+    }
 
 }
