8c42afbf0c7c70ffd25404e4edea323d4311d9b6
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-03-02 20:54:01 +0900

    Spring -> Config : bean 업데이트

--- Spring/src/main/java/CarrotMarket/CarrotMarket/Config.java
+++ Spring/src/main/java/CarrotMarket/CarrotMarket/Config.java
@@ -1,7 +1,10 @@
 package CarrotMarket.CarrotMarket;
 
+import CarrotMarket.CarrotMarket.repository.BoardRepository;
 import CarrotMarket.CarrotMarket.repository.MemberRepository;
+import CarrotMarket.CarrotMarket.repository.MemoryBoardRepository;
 import CarrotMarket.CarrotMarket.repository.MemoryMemberRepository;
+import CarrotMarket.CarrotMarket.service.ListService;
 import CarrotMarket.CarrotMarket.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,5 +20,15 @@ public class Config {
     @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public ListService listService(){
+        return  new ListService((boardRepository()));
+    }
+
+    @Bean
+    public BoardRepository boardRepository(){
+        return new MemoryBoardRepository();
     }
 }
