45107a7cb7b9c9119270c17d1c582ea77c365d97
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-03-02 20:56:11 +0900

    Android -> board : board 데이터 클래스 생성

---
+++ Android/app/src/main/java/com/example/carrotmarket/dto/Board.kt
@@ -0,0 +1,16 @@
+package com.example.carrotmarket.dto
+
+data class Board(
+        var id : Int,
+        var price: Long,
+        var title: String,
+        var text: String,
+        var categoryId: Int,
+        var location: String,
+        var nickname: String,
+        var registerDate: String,
+        var deadlineDate: String,
+        var dibsCnt: Int,
+        var viewCnt: Int,
+        var chatCnt: Int
+)
