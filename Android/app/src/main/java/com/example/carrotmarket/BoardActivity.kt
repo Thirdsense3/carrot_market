90fc6e1d202b991568cda49b3e98fa63e33f8ca3
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-03-02 20:53:40 +0900

    Android : boardList Rest API 추가 및 코루틴 함수 작성

--- Android/app/src/main/java/com/example/carrotmarket/BoardActivity.kt
+++ Android/app/src/main/java/com/example/carrotmarket/BoardActivity.kt
@@ -2,12 +2,42 @@ package com.example.carrotmarket
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.carrotmarket.dto.Board
+import com.example.carrotmarket.network.RetrofitClient
+import com.example.carrotmarket.network.RetrofitService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BoardActivity: AppCompatActivity() {
     private val TAG = "BoardActivity"
+    lateinit var board: Board
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_boardlist)
+        val url = "http://10.0.2.2:8080/login"
+        val retrofit = RetrofitClient.getInstance()
+        val myAPI = retrofit.create(RetrofitService::class.java)
+
+
+        CoroutineScope(IO).launch {
+            myAPI.boardList().enqueue(object : Callback<Board>{
+                override fun onFailure(call: Call<Board>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onResponse(call: Call<Board>, response: Response<Board>) {
+                    response.body()?.let {
+                        board = Board(it.id, it.price,it.title,it.text,it.categoryId,it.location,it.nickname,it.registerDate,it.deadlineDate,it.dibsCnt,it.viewCnt,it.chatCnt)
+                    }
+                }
+            })
+
+        }
     }
+
 }
