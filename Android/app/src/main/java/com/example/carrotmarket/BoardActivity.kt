edb637ebe686c3176943d6781e44fd322c078851
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-06-03 16:26:24 +0900

    ViewPager2 적용
    ViewPager2 적용 및 코드 변경

--- Android/app/src/main/java/com/example/carrotmarket/BoardActivity.kt
+++ Android/app/src/main/java/com/example/carrotmarket/BoardActivity.kt
@@ -18,17 +18,35 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+
+
+private const val NUM_PAGES = 5
+private const val TAG = "BoardActivity"
 
 class BoardActivity: AppCompatActivity() {
     private val retrofit = RetrofitClient.getInstance()
     private val myAPI: RetrofitService = retrofit.create(RetrofitService::class.java)
-    private val TAG = "BoardActivity"
     lateinit var board: Board
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private lateinit var viewPager: ViewPager2
 
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board)
+
+        viewPager = findViewById(R.id.boardPager)
+
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
 
         val toolbar:Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -161,6 +179,28 @@ class BoardActivity: AppCompatActivity() {
         })
 
         return board
+    }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment = ScreenSlidePageFragment()
     }
 
 }
