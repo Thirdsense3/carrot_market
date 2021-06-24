9af69d36b0ce8fc48b8961f82a95e90941cd6317
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-06-24 17:52:39 +0900

    Android : User, BaseMessage Dto 삭제
    DTO 변경으로 인한 삭제

--- Android/app/src/main/java/com/example/carrotmarket/dto/chat/BaseMessage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.carrotmarket.dto.chat
-
-import com.example.carrotmarket.dto.chat.User
-
-class BaseMessage {
-    var message: String? = null
-    var sender: User? = null
-    var createdAt: Long = 0
-}
--- Android/app/src/main/java/com/example/carrotmarket/dto/chat/User.kt
+++
@@ -1,6 +0,0 @@
-package com.example.carrotmarket.dto.chat
-
-class User {
-    var nickname: String? = null
-    var profileUrl: String? = null
-}
