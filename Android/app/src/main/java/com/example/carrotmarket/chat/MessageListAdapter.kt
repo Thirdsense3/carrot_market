bd20ed1827640e5b160d45df938bdd4530a12a24
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-06-24 17:56:08 +0900

    Update MessageListAdapter.kt
    DTO 변경

--- Android/app/src/main/java/com/example/carrotmarket/chat/MessageListAdapter.kt
+++ Android/app/src/main/java/com/example/carrotmarket/chat/MessageListAdapter.kt
@@ -3,9 +3,9 @@ package com.example.carrotmarket.chat
 import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
 import com.example.carrotmarket.RecyclerAdapter
-import com.example.carrotmarket.dto.chat.BaseMessage
+import com.example.carrotmarket.dto.chat.ChatMessage
 
 abstract class MessageListAdapter(
     private val mContext: Context,
-    private val mMessageList: List<BaseMessage>
+    private val mMessageList: List<ChatMessage>
 ) : RecyclerView.Adapter<RecyclerAdapter.Holder>()
